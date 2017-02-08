e4525cf7e382fa2358f6b117893e5a887bd5bf50
Ben Rees <brees@fraedom.com>
2017-02-09 10:54:15 +1300

    Add example 5

---
+++ AsyncIsNotConcurrent/Example5InvokeThenWait.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncIsNotConcurrent
+{
+    public class Example5InvokeThenWait
+    {
+        public void MainMethod()
+        {
+            var child = WorkThenWait();
+            Console.WriteLine("Started.");
+            child.Wait();
+            Console.WriteLine("Completed.");
+        }
+
+        private async Task WorkThenWait()
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("doing work...");
+            await Task.Delay(1000);
+        }
+    }
+}
---
+++ AsyncIsNotConcurrent/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AsyncIsNotConcurrent
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Example5().MainMethod();
+            // new Example2CatchingExceptions().MainMethod();
+            // new SolutionCatchingExceptions().MainMethod().Wait();
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine(); // Unhandled exceptions above may mean ReadLine is unavailable.
+        }
+    }
+}
