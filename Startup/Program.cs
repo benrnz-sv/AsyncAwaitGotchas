70755ab53c475a4dff126e9faff3f4af01bdaca7
Ben Rees <brees@fraedom.com>
2017-02-09 16:06:54 +1300

    Start up app

---
+++ Startup/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Startup
+{
+    public static class Program
+    {
+        private static readonly string Data1 =
+            "While Async/Await does simplify and make your code better, developers must be aware of some crucial best practices.";
+
+        private static readonly string Data2 =
+            "It can sometimes have seemingly unpredictable results.";
+
+        private static Random Random = new Random(DateTime.Now.Second);
+
+        private const int MaxPause = 4000;
+
+        public static void Main(string[] args)
+        {
+            StartupDemo().Wait();
+            Console.WriteLine();
+        }
+
+        private static async Task StartupDemo()
+        {
+            var sentence1 = Data1.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var sentence2 = Data2.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var abort = true;
+
+            do
+            {
+                Console.Clear();
+                abort = await OutputSentence(sentence1);
+                if (abort) break;
+                Console.WriteLine();
+                Console.WriteLine();
+                abort = await OutputSentence(sentence2, MaxPause);
+            } while (!abort);
+        }
+
+        private static async Task<bool> OutputSentence(string[] sentence2, int maxPause = 0)
+        {
+            await Task.Delay(MaxPause);
+            foreach (var word in sentence2)
+            {
+                Console.Write($"{word} ");
+                await Task.Delay(maxPause == 0 ? 2000 : Random.Next(maxPause));
+                if (Console.KeyAvailable)
+                {
+                    return true;
+                }
+            }
+
+            await Task.Delay(MaxPause);
+            return false;
+        }
+    }
+}
